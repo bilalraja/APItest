32e3c4a7a448e8d1955d36af406de82885f9c9fe
Bilal Raja <bilal.raja.iqbal@gmail.com>
2021-04-06 17:23:28 +0200

    Add files via upload

---
+++ Data1.js
@@ -0,0 +1,75 @@
+[
+    {
+      id: "1", 
+      image: 'https://images.pexels.com/photos/754942/pexels-photo-754942.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      title: "Kunstner",
+      description: "Kunstverk 1",
+      description1:'Statue er en skulpturell avbildning av en skikkelse, ofte menneske eller dyr. Statuer var vanlige gjennom hele antikken, idag er de vanlige innslag i parker og andre offentlige miljøer. Vanligvis oppføres statuer for å hedre minnet av en historisk hendelse eller en betydningsfull person.',
+      image1: 'https://images.unsplash.com/photo-1577086664693-894d8405334a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=933&q=80',
+    },
+    {
+      id: "2",
+      title: "Kunstner",
+      image: 'https://images.pexels.com/photos/189528/pexels-photo-189528.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      description: "Kunstverk 2",
+      description1: "Statue er en skulpturell avbildning av en skikkelse, ofte menneske eller dyr. Statuer var vanlige gjennom hele antikken, idag er de vanlige innslag i parker og andre offentlige miljøer. Vanligvis oppføres statuer for å hedre minnet av en historisk hendelse eller en betydningsfull person. ",
+      image1: 'https://images.unsplash.com/photo-1571172964533-d2d13d88ce7e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80',
+    },
+    {
+      id: "3",
+      title: "Kunstner",
+      image: 'https://images.unsplash.com/photo-1615033435768-20052d23babe?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1189&q=80',
+      description: "Kunstverk 3",
+      description1: "Statue er en skulpturell avbildning av en skikkelse, ofte menneske eller dyr. Statuer var vanlige gjennom hele antikken, idag er de vanlige innslag i parker og andre offentlige miljøer. Vanligvis oppføres statuer for å hedre minnet av en historisk hendelse eller en betydningsfull person. ",
+      image1: 'https://images.pexels.com/photos/754942/pexels-photo-754942.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    },
+    {
+      id: "4",
+      title: "Kunstner", 
+      image: 'https://images.unsplash.com/photo-1588904570641-eb0efcf4d369?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+      description: "Kunstverk 4",
+      description1: "Statue er en skulpturell avbildning av en skikkelse, ofte menneske eller dyr. Statuer var vanlige gjennom hele antikken, idag er de vanlige innslag i parker og andre offentlige miljøer. Vanligvis oppføres statuer for å hedre minnet av en historisk hendelse eller en betydningsfull person. "    
+    },
+    {
+      id: "5",
+      title: "Kunstner",
+      image: 'https://images.unsplash.com/photo-1615312576296-8681ec411e4e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+      description: "Kunstverk 5",
+      description1: "Statue er en skulpturell avbildning av en skikkelse, ofte menneske eller dyr. Statuer var vanlige gjennom hele antikken, idag er de vanlige innslag i parker og andre offentlige miljøer. Vanligvis oppføres statuer for å hedre minnet av en historisk hendelse eller en betydningsfull person. "    
+    },
+    {
+      id: "6",
+      title: "Kunstner",
+      image: 'https://images.unsplash.com/photo-1603647520576-09e067c68687?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=793&q=80',
+      description: "Kunstverk 6",
+      description1: "Statue er en skulpturell avbildning av en skikkelse, ofte menneske eller dyr. Statuer var vanlige gjennom hele antikken, idag er de vanlige innslag i parker og andre offentlige miljøer. Vanligvis oppføres statuer for å hedre minnet av en historisk hendelse eller en betydningsfull person. "
+    },
+    {
+      id: "7",
+      title: "Kunstner",
+      image: 'https://images.unsplash.com/photo-1588240668824-773bccf880fb?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+      description: "Kunstverk 7",
+      description1: "Statue er en skulpturell avbildning av en skikkelse, ofte menneske eller dyr. Statuer var vanlige gjennom hele antikken, idag er de vanlige innslag i parker og andre offentlige miljøer. Vanligvis oppføres statuer for å hedre minnet av en historisk hendelse eller en betydningsfull person. "    
+    },
+    {
+      id: "8",
+      title: "Kunstner",
+      image: 'https://images.unsplash.com/photo-1529154166925-574a0236a4f4?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=967&q=80',
+      description: "Kunstverk 8",
+      description1: "Statue er en skulpturell avbildning av en skikkelse, ofte menneske eller dyr. Statuer var vanlige gjennom hele antikken, idag er de vanlige innslag i parker og andre offentlige miljøer. Vanligvis oppføres statuer for å hedre minnet av en historisk hendelse eller en betydningsfull person. "    
+    },
+    {
+      id: "9",
+      title: "Kunstner",
+      image: 'https://images.pexels.com/photos/5214869/pexels-photo-5214869.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      description: "Kunstverk 9",
+      description1: "Statue er en skulpturell avbildning av en skikkelse, ofte menneske eller dyr. Statuer var vanlige gjennom hele antikken, idag er de vanlige innslag i parker og andre offentlige miljøer. Vanligvis oppføres statuer for å hedre minnet av en historisk hendelse eller en betydningsfull person. "    
+    },
+    {
+      id: "10",
+      title: "Kunstner",
+      image: 'https://images.pexels.com/photos/1478243/pexels-photo-1478243.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      description: "Kunstverk 10",
+      description1: "Statue er en skulpturell avbildning av en skikkelse, ofte menneske eller dyr. Statuer var vanlige gjennom hele antikken, idag er de vanlige innslag i parker og andre offentlige miljøer. Vanligvis oppføres statuer for å hedre minnet av en historisk hendelse eller en betydningsfull person. "    
+    },
+  ]
